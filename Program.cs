764fd222ac841a84c6dec38df8af6fd66bbbaf7e
Maria Eduarda Bedetti Amorim <Conta.para.qualquer.coisa2005@gmail.com>
2022-05-09 05:21:03 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int numeroDigitado;
+Console.Write("Digite um número: ");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+bool numeroEhPar = (numeroDigitado % 2) == 0;
+
+if (numeroEhPar)
+{
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+     Console.WriteLine($"{numeroDigitado} é par");
+}
+
+else 
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow; 
+    Console.WriteLine ($"{numeroDigitado} é ímpar");
+}
+
+Console.ResetColor();
